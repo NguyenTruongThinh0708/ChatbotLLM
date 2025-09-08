6fcff7442267bb1fce00ff9afe3e4aba88645937
NGUYEN TRUONG THINH <nguyentruongthinht91@gmail.com>
2025-09-08 15:48:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # setup.sh - chuẩn bị môi trường cho Streamlit Cloud
 
-# Cài JDK (phòng khi packages.txt không chạy kịp)
 sudo apt-get update -y
 sudo apt-get install -y openjdk-17-jdk
 
-# Đặt JAVA_HOME cho openjdk-17
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-echo "JAVA_HOME set to $JAVA_HOME"
+echo "JAVA_HOME=$JAVA_HOME"
 java -version
 javac -version
+which java
+which javac
