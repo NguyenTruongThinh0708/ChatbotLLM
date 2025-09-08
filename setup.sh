3927ff887d4f2da15974da833eae1ee785823a3f
NGUYEN TRUONG THINH <nguyentruongthinht91@gmail.com>
2025-09-08 15:45:23 +0700

    Install OpenJDK 17 in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # setup.sh - chuẩn bị môi trường cho Streamlit Cloud
 
+# Cài JDK (phòng khi packages.txt không chạy kịp)
+sudo apt-get update -y
+sudo apt-get install -y openjdk-17-jdk
+
 # Đặt JAVA_HOME cho openjdk-17
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
