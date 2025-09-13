4c7596927e3b8817c9966c85029ade994d3b1486
NGUYEN TRUONG THINH <nguyentruongthinht91@gmail.com>
2025-09-13 10:04:51 +0700

    Update email_settings.py

--- email_settings.py
+++ email_settings.py
@@ -1,8 +1,8 @@
 # email_settings.py
 
 # Email dùng để gửi
-from_email_default = "phongvandaide@gmail.com"
-password_default = "tmjm enzb ryqw pdzm"  # Mật khẩu ứng dụng
+from_email_default = "millerken5604@gmail.com"
+password_default = "iinl vivv zpfg chjf"  # Mật khẩu ứng dụng
 
 # Cấu hình SMTP server
 HOST = "smtp.gmail.com"  # hoặc server khác nếu không dùng Gmail
