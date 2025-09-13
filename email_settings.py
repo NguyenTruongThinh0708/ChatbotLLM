557f4daf9d09870885e5e3c618ff34b31c677089
NGUYEN TRUONG THINH <nguyentruongthinht91@gmail.com>
2025-09-13 09:38:18 +0700

    Add files via upload

---
+++ email_settings.py
@@ -0,0 +1,9 @@
+# email_settings.py
+
+# Email dùng để gửi
+from_email_default = "phongvandaide@gmail.com"
+password_default = "tmjm enzb ryqw pdzm"  # Mật khẩu ứng dụng
+
+# Cấu hình SMTP server
+HOST = "smtp.gmail.com"  # hoặc server khác nếu không dùng Gmail
+PORT = 587
